2f4213391e2ee5a392f6df7332fccc5d30e9a7a4
lyjoice <lahely.na@gmail.com>
2025-07-12 06:44:48 +0200

    images et index html

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+
 function toggleTable() {
+      const table = document.getElementById("ranch-table");
+      if (table.style.display === "none" || table.style.display === "") {
+        table.style.display = "block";
+      } else {
+        table.style.display = "none";
+      }
+    }
+
+    /*function toggleTable() {
   const table = document.getElementById("us-table");
   table.style.display = table.style.display === "none" ? "table" : "none";
-}
+}*/
